81c6a02875b7d97f6806719916267af5c6b411f2
jjtagaca <70424192+jjtagaca@users.noreply.github.com>
2024-02-12 19:54:48 +0800

    Add files via upload

---
+++ 01-console/script.js
@@ -0,0 +1,16 @@
+console.log(100);
+console.log('Hello World');
+console.log(20, 'Hello', true);
+const x = 100;
+console.log(x);
+console.error('Alert');
+console.warn('Warning');
+console.table({name: 'James', email: 'jjtagaca@gmail.com'})
+console.group('sample');
+console.log(x);
+console.error('Alert');
+console.warn('Warning');
+console.groupEnd
+
+const styles = 'padding: 10px; background-color: white; color: green';
+console.log('%cHello World',styles);
